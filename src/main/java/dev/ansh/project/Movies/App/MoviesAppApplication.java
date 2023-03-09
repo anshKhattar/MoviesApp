31bab687bf9b24e9cd821074260d5eb3dd0bdb67
anshKhattar <akhattar@tekion.com>
2023-03-09 15:22:04 +0530

    initialised repo

---
+++ src/main/java/dev/ansh/project/Movies/App/MoviesAppApplication.java
@@ -0,0 +1,13 @@
+package dev.ansh.project.Movies.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ansh/project/Movies/App/MoviesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ansh.project.Movies.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
